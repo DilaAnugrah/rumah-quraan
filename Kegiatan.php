ad64cfb9dc6aba94848905d94c616124d76e4158
DilaAnugrah <118800510+DilaAnugrah@users.noreply.github.com>
2022-11-22 14:55:04 +0700

    Add files via upload

---
+++ Kegiatan.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <title>Rumah Quran Asy-Syafi'i</title>
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <!-- awal Navbar -->
+        <nav class="navbar navbar-expand-lg fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="index.php"><img src="Gambar/logo.jpeg" alt="" height="75" width="175"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavAltMarkup" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup" id="navbarNavDarkDropdown">
+                    <div class="navbar-nav ms-auto">
+                        <a class="nav-link active" aria-current="page" href="index.php" style="color: white;">Beranda</a>
+                        <a class="nav-link active" aria-current="page" href="Kegiatan.php" style="color: white;"><b><u>Kegiatan</u></b></a>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: white;">
+                                Informasi
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu" style="background-color: #527f3a;">
+                                <li><a class="dropdown-item" href="Data_Siswa.php" style="color: white;">Data Siswa</a></li>
+                                <li><a class="dropdown-item" href="Data_Pengajar.php" style="color: white;">Data Pengajar</a></li>
+                            </ul>
+                        </li>
+                        <a class="nav-link active" aria-current="page" href="Login.php" style="color: white;">Login</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <!-- akhir Navbar -->
+
+        <!-- Kegiatan -->
+        <div class="kegiatan">
+            <br> <br> <br> <br>
+            <h2>Kegiatan</h2>
+            <br><br><br>
+            <div class="row justify-content-center row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/kegiatan1.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/kegiatan2.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a short card.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/kegiatan3.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/Kegiatan 4.jfif" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/Kegiatan 5.jfif" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div><div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/Kegiatan 6.jfif" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div><div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/Kegiatan 7.jfif" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div><div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/Kegiatan 8.jfif" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Akhir Kegiatan -->
+
+    </div>
+
+        <!-- footer -->
+        <footer>
+            <div class="container pb-4">
+                <h2 style="padding-top: 3%;">Rumah Quran Asy-Syafi'i</h2>
+                <br>
+                <img src="Gambar/home.svg" alt="" style="padding-right: 10px;">Jl. Gatot Subroto, Bengkalis Sebelum Simapng 3 Hangtuah
+                <br> <br>
+                <img src="Gambar/whatsapp-svgrepo-com.svg" alt="" style="padding-right: 10px;">0823-8521-4529
+                <br> <br>
+                <img src="Gambar/facebook-svgrepo-com.svg" alt=""  style="padding-right: 10px; height: 20px;"> RumahQuran Asy Syafii
+                <br><br>
+                
+            </div>
+        </footer>
+        <!-- akhir footer --
+</body>
+</html>
