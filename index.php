d8b7ea252657e7207fe877f6ef49f7e5ffd6f004
DilaAnugrah <118800510+DilaAnugrah@users.noreply.github.com>
2022-11-22 14:55:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <title>Rumah Quran Asy-Syafi'i</title>
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <!-- awal Navbar -->
+        <nav class="navbar navbar-expand-lg fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="index.php"><img src="Gambar/logo.jpeg" alt="" height="75" width="175"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavAltMarkup" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup" id="navbarNavDarkDropdown">
+                    <div class="navbar-nav ms-auto">
+                        <a class="nav-link active" aria-current="page" href="index.php" style="color: white;"><b><u>Beranda</b></u></a>
+                        <a class="nav-link active" aria-current="page" href="Kegiatan.php" style="color: white;">Kegiatan</a>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: white;">
+                                Informasi
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu" style="background-color: #527f3a;">
+                                <li><a class="dropdown-item" href="Data_Siswa.php" style="color: white;">Data Siswa</a></li>
+                                <li><a class="dropdown-item" href="Data_Pengajar.php" style="color: white;">Data Pengajar</a></li>
+                            </ul>
+                        </li>
+                        <a class="nav-link active" aria-current="page" href="Login.php" style="color: white;">Login</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <!-- akhir Navbar -->
+    </div>
+
+        <!-- Image -->
+        <div class="headline">
+            <img src="Gambar/Headline.jpeg" alt="">
+        </div>
+        <!-- Image -->
+
+    <div class="container">    
+        <!-- introduction -->
+        <div class="introduction">
+            <h3 style="text-align: center;">Rumah Quran Asy-Syafi'i</h3>
+            <p style="text-align: justify;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio necessitatibus cumque sed aut ipsam quo delectus ipsa ut voluptas, quibusdam porro consequatur, aperiam nam magni neque nobis asperiores error enim? Ipsum quae ea, assumenda officiis maxime facilis repudiandae consequuntur! Cupiditate aliquid placeat doloremque adipisci reiciendis eius at molestias asperiores, dolor dicta, sapiente ea iure laudantium quod facere eos perspiciatis ducimus consectetur doloribus totam sequi, vel odit magni vero! Corporis eum architecto, at accusantium veniam doloremque labore distinctio incidunt esse autem expedita aliquid! Sit aperiam nemo aliquid earum harum vitae repellendus magni nobis! Quidem aspernatur consequuntur ipsam culpa assumenda fuga voluptas. Corporis quod molestias, iusto, ratione illo aperiam mollitia culpa necessitatibus delectus aut sint ipsa blanditiis earum expedita sapiente impedit repellendus debitis. Quasi voluptatum amet atque recusandae possimus dignissimos nobis fuga similique id alias quam sunt, praesentium dolorum maiores ullam rem molestias. Commodi accusantium excepturi aliquam. Iste ab quam corporis nesciunt culpa molestiae delectus blanditiis distinctio accusamus quas! Voluptatum aut provident nostrum molestiae deserunt commodi similique repellat nesciunt eveniet quis atque delectus officiis assumenda dicta porro asperiores natus, voluptatibus hic? Necessitatibus eveniet cum commodi repellat autem maxime optio sed nostrum ut ipsam. Tempore accusantium architecto soluta. Rerum temporibus sit blanditiis. Ullam quasi quo, vitae laudantium nisi quisquam molestias sit minus provident voluptas voluptates quae enim eius accusamus, eos architecto odit nihil id sunt dicta facilis inventore omnis officia? Consequatur enim dolorem ab porro repellendus asperiores, accusamus harum quidem soluta, aperiam nulla odio omnis exercitationem voluptatum. Pariatur quod doloremque deserunt similique laudantium neque ullam expedita numquam veniam ipsam maxime, porro tempora eos nihil nemo eaque quas excepturi? Esse ipsum reiciendis nemo odit delectus voluptates eum maxime amet iure, ab illum consequatur optio quia? Illum, nihil. Architecto fugit voluptatem blanditiis delectus maiores similique. Quos, magnam fugiat odit voluptas nihil voluptatum ut quae quisquam! Fugit debitis ea aliquid pariatur temporibus excepturi officiis error labore inventore autem distinctio sit cupiditate veniam adipisci necessitatibus, consequatur dignissimos sequi sunt eos! Dolorum ducimus quis commodi vel, iure illo delectus corrupti obcaecati. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem corporis sequi provident at rem quisquam, sit eum perspiciatis quo delectus. Saepe nihil voluptatibus aspernatur dicta? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta officiis ea molestias tenetur eius. Alias impedit debitis ab ipsum id, corporis assumenda, voluptas reiciendis sit eaque similique, natus voluptate quo vel magni omnis sed cupiditate voluptatem repudiandae aliquam esse hic odio. Molestiae, numquam officia laboriosam commodi voluptatibus eveniet aut. Soluta.</p>
+        </div>
+        <!-- akhir Introduction -->
+
+        <!-- Kegiatan -->
+        <div class="kegiatan">
+            <h3 style="text-align: center;">Kegiatan</h3>
+            <div class="row justify-content-center row-cols-1 row-cols-md-3 g-4 pt-5">
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/kegiatan1.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/kegiatan2.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a short card.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/kegiatan3.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/Kegiatan 4.jfif" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="Gambar/Kegiatan 5.jfif" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Akhir Kegiatan -->
+    </div>
+
+        <!-- footer -->
+        <footer>
+            <div class="container pb-4">
+                <h2 style="padding-top: 3%;">Rumah Quran Asy-Syafi'i</h2>
+                <br>
+                <img src="Gambar/home.svg" alt="" style="padding-right: 10px;">Jl. Gatot Subroto, Bengkalis Sebelum Simapng 3 Hangtuah
+                <br> <br>
+                <img src="Gambar/whatsapp-svgrepo-com.svg" alt="" style="padding-right: 10px;">0823-8521-4529
+                <br> <br>
+                <img src="Gambar/facebook-svgrepo-com.svg" alt=""  style="padding-right: 10px; height: 20px;"> RumahQuran Asy Syafii
+                <br><br>
+                
+            </div>
+        </footer>
+        <!-- akhir footer -->
+</body>
+</html>
